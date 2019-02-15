818b4f0b0a91a4b293f97f314744e390ddfede7e
davidanew <davidanew@googlemail.com>
2019-02-15 11:08:45 +0000

    getImage working for unit test

--- roblox_limited_items/ImageInterface.swift
+++ roblox_limited_items/ImageInterface.swift
@@ -5,10 +5,26 @@ import Alamofire
 
 // handle calls to get image objects
 class ImageInterface {
-    func getImage(url : String, row : Int , callBack : (Int) -> Void) {
-        print("will attempt to get image \(url)")
-        callBack(row)
+    // dictionary to act as cache for images
+    var imageCache: [String:UIImage] = [:]
+
+    func getImage(url : String, row : Int , callBack : @escaping (Int) -> Void) -> UIImage? {
+        if let image = imageCache[url] {
+            print("image in cache")
+            return image
+        }
+        else {
+            print("image not in cache")
+
+            Alamofire.request(url).response{ response in
+                print("alamofire cb")
+                if let data = response.data{
+                    let image = UIImage(data:data)
+                    self.imageCache[url] = image
+                    callBack(row)
+                }
+            }
+            return nil
+        }
     }
-    
-    
 }
