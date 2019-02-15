690487c26df3927b6cb35dfd8722102d8803a168
davidanew <davidanew@googlemail.com>
2019-02-15 09:48:23 +0000

    Removed commented code and updated comments

--- roblox_limited_items_tests/ApiInterfaceTests.swift
+++ roblox_limited_items_tests/ApiInterfaceTests.swift
@@ -5,84 +5,38 @@ import SwiftyJSON
 @testable import roblox_limited_items
 
 class ApiInterfaceTests: XCTestCase {
+    //class to test ApiInterface
     
     // greate object of class under test
     let iut = ApiInterface()
     // getlatestCollectables is asyncronous so needs a callback function
     // this is the expectation handler for this
     let getLatestCollectablesExpectation = XCTestExpectation(description: "getLatestCollectablesExpectation")
-    // jsonAlamofire is asycncronous and needs a callback function
-//    let jsonAlamofireExpectation = XCTestExpectation(description: "jsonAlamofireExpectation")
-    
-    //instructions https://developer.roblox.com/articles/Catalog-API
+    //TODO get this from apiInterface
     let numCollectibles = 30
-//    let urlLatestCollectables =  "https://search.roblox.com/catalog/json?SortType=RecentlyUpdated&IncludeNotForSale=false&Category=Collectibles&ResultsPerPage=30"
-//    let urlLatestAll =  "https://search.roblox.com/catalog/json?SortType=RecentlyUpdated&IncludeNotForSale=false&ResultsPerPage=30"
-//    let urlBitcoin = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"
     
-//    var jsonLatestCollectables : JSON?
- /*
-    func testJsonAlamofire() {
-        print("Running testJsonAlamofire")
-        let url = urlLatestCollectables
-        iut.jsonAlamofire(url: url, jsonHandler: didJsonAlamofire, rootCompletionHandler: nil)
-        // wait for callback didJsonAlamofire
-        wait(for: [jsonAlamofireExpectation], timeout: 5)
-        //unwrap json
-        if let json = jsonLatestCollectables {
-            //TODO this should not depend on count
-            let numItems = json.count
-            // this just for looking at the output, not needed for testing?
-            for itemNum in 0..<numItems{
-                print (" \(  json[itemNum]["Updated"].stringValue) \(  json[itemNum]["IsLimitedUnique"].stringValue) \(  json[itemNum]["IsLimited"].stringValue) \(  json[itemNum]["Name"].stringValue)")
-            }
-            for itemNum in 0..<5{
-                XCTAssertTrue(json[itemNum]["IsLimitedUnique"].stringValue == "true" || json[itemNum]["IsLimited"].stringValue == "true")
-            }
-        }
-        else {
-            print ("JSON error")
-            XCTFail()
-        }
-        // TODO check isLimited or isLimitedUnique true
-        
-    }
-    
-    // callback for testJsonAlamofire
-    func didJsonAlamofire(json : JSON? , getLatestCollectablesCompletionHandler : (([String]) -> Void)? ) {
-
-        jsonLatestCollectables = json
-        jsonAlamofireExpectation.fulfill()
-    }
-*/
-    
-    // test getLatestCollectables in ApiInterface
+    // test getLatestCollectables
     func testGetLatestCollectables() {
-        print("Running testGetLatestCollectablesList")
+        print("Running testGetLatestCollectables")
         // run funnction, it needs the name of the callback
+        // todo put in closure
         iut.getLatestCollectables(completionHandler: getLatestCollectablesCompletionHandler)
-        //the funtion will call getLatestCollectablesCompletionHandler, which will fulfill the expectation
+        //the function will call getLatestCollectablesCompletionHandler, which will fulfill the expectation
         wait(for: [getLatestCollectablesExpectation], timeout: 5)
-//        print(iut.jsonLatestCollectables)
+        // check JSON is not nil
         XCTAssertNotNil(iut.getNumEntries())
-            for itemNum in 0..<numCollectibles{
-//                XCTAssertTrue(json[itemNum]["IsLimitedUnique"].stringValue == "true" || json[itemNum]["IsLimited"].stringValue == "true")
-                XCTAssertTrue(iut.getIsLimitedUnique(index : itemNum) == "true" || iut.getIsLimited(index : itemNum) == "true")
-                XCTAssertNotNil(iut.getName(index : itemNum))
-                XCTAssertNotNil(iut.getUpdated(index : itemNum))
-
-                
-//                print("testing item \(itemNum)")
-//                print(iut.getIsLimitedUnique(index: itemNum))
-                
-            }
-//        }
-//        else {
-//            print ("JSON error")
-//            XCTFail()
-//        }
+        // loop through all items
+        for itemNum in 0..<numCollectibles{
+            //theck that this is a limited item
+            XCTAssertTrue(iut.getIsLimitedUnique(index : itemNum) == "true" || iut.getIsLimited(index : itemNum) == "true")
+            //check the other getters
+            //TODO add missing getter tests
+            XCTAssertNotNil(iut.getName(index : itemNum))
+            XCTAssertNotNil(iut.getUpdated(index : itemNum))
+        }
     }
     
+    //TODO look at closures and tidy up
     // the callback for testGetLatestCollectablesList
     func getLatestCollectablesCompletionHandler(catalog : [String]) {
         print("callback successful, recieved \(catalog)")
