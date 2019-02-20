d441321ca2493d63c2c800898f0cd4136ea0b550
davidanew <davidanew@googlemail.com>
2019-02-20 09:15:47 +0000

    Changed case of labels Added code to get large thumbnail

--- roblox_limited_items/ItemDetailVC.swift
+++ roblox_limited_items/ItemDetailVC.swift
@@ -6,17 +6,19 @@ import SwiftyJSON
 class ItemDetailVC: UIViewController, setLatestCollectablesDelegate{
     //need API interface to hold data and use functions for retieval
     var apiInterface = ApiInterface()
+    var imageInterface = ImageInterface()
     // This is the row in the collectables data that the item is at
     var rowInData : Int?
     
     // At the moment just output data to labels
-    @IBOutlet weak var Label1: UILabel!
-    @IBOutlet weak var Label2: UILabel!
-    @IBOutlet weak var Label3: UILabel!
-    @IBOutlet weak var Label4: UILabel!
-    @IBOutlet weak var Label5: UILabel!
-    @IBOutlet weak var Label6: UILabel!
-    @IBOutlet weak var Label8: UILabel!
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    @IBOutlet weak var label6: UILabel!
+    @IBOutlet weak var label7: UILabel!
     
     // This function used to send JSON data into this instance of apiInterface
     // Also need row for the item of interest
@@ -31,11 +33,34 @@ class ItemDetailVC: UIViewController, setLatestCollectablesDelegate{
         super.viewWillAppear(animated)
         // try out putting the value sin the labels
         if let row = rowInData {
-            Label1.text = apiInterface.getName(index: row)
-            Label2.text = apiInterface.getRemaining(index: row)
-            Label3.text = apiInterface.getPrice(index: row)
-            Label4.text = apiInterface.getUpdated(index: row)
-            Label5.text = apiInterface.getIsForSale(index: row)
+ /*           if let imageUrl = apiInterface.getLargeThumbnailData(index: row){
+                //row could be optional here as not needed for non table view
+                image1.image = imageInterface.getImage(url: imageUrl, row: row, closure: { (row) in
+                    self.image1.image = self.imageInterface.getImage(url: imageUrl, row: row, closure: { (row) in
+                        print("double image cache miss")
+                    })
+                })
+            } */
+            
+            // TODO: put this code in test!
+            // may need to daisy chain with expectations
+            apiInterface.retrieveLargeThumbnailData(index: row) { (success) in
+                // if success
+                if let imageUrl = self.apiInterface.getLargeThumbnailUrl() {
+                    self.image1.image = self.imageInterface.getImage(url: imageUrl, row: row, closure: { (row) in
+                        self.image1.image = self.imageInterface.getImage(url: imageUrl, row: row, closure: { (row) in
+                            print("double image cache miss")
+                        })
+                    })
+                    
+                }
+            }
+            
+            label1.text = apiInterface.getName(index: row)
+            label2.text = apiInterface.getDescription(index: row)
+            label3.text = apiInterface.getPrice(index: row)
+            label4.text = apiInterface.getUpdated(index: row)
+            label5.text = apiInterface.getIsForSale(index: row)
             
             
         }
