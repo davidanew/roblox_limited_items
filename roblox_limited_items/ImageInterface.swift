be35946e2a00750667e3e9ad4b89b1fb15e393bc
davidanew <davidanew@googlemail.com>
2019-02-22 09:36:28 +0000

    Comment changes

--- roblox_limited_items/ImageInterface.swift
+++ roblox_limited_items/ImageInterface.swift
@@ -8,7 +8,7 @@ class ImageInterface {
     // dictionary to act as cache for images
     var imageCache: [String:UIImage] = [:]
     
-    // This funtion returns the image if it is in the cache. Otherwise it calls alomofire with user supplied callback
+    // This function returns the image if it is in the cache. Otherwise it calls alomofire with user supplied callback
     // this callback most likely will trigger a refresh
     func getImage(url : String, row : Int , closure : @escaping (Int) -> Void) -> UIImage? {
         if let image = imageCache[url] {
@@ -19,17 +19,16 @@ class ImageInterface {
         else {
             // Image is not in the cache
             Alamofire.request(url).response{ response in
- //             // alamofire callback
                 if let data = response.data{
                     // data is not nil
                     let image = UIImage(data:data)
                     // put image in cache
                     self.imageCache[url] = image
                     // callBack to say data is valid
+                    // not returning any success status as doesn't matter
+                    // if image doesn't load closure is not run
                     closure(row)
                 }
-                //TODO - some error checking, is there a timout from alomofire
-                //Do we need this?
             }
             return nil
         }
