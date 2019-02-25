a1165f370937f5e3815a41a76934d70506d57e0f
davidanew <davidanew@googlemail.com>
2019-02-25 09:37:35 +0000

    update plan

--- roblox_limited_items/Plan.swift
+++ roblox_limited_items/Plan.swift
@@ -1,14 +1,7 @@
 //  Copyright © 2019 David New. All rights reserved.
 
-// weak and no wifi tests:
-// look at watchdog timers
-//combine refersh functions
-// check against roblox website
-// look up limited unique and unique
-// need images
-// dummy image for unfilled cells
-// test with james and ali
-// what about roblox copyright
+// test 
+
 
 
 
